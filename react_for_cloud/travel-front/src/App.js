b9976a4489365c16041338d8ab724ca7e9b23476
prsstromz <naruto31110@hotmail.com>
2022-12-05 17:19:23 +0700

    create frontend react call api

---
+++ react_for_cloud/travel-front/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react'
+
+function App() {
+    const [items, setItems] = useState([])
+
+    useEffect(() => {
+        fetch('https://cautious-lion-mittens.cyclic.app/attraction')
+    .then(res => res.json())
+	.then((result) => {
+	console.log(result)
+	setItems(result)
+	})
+	}, [])
+
+    return (
+	<div>
+	<h1>travel api</h1>
+	<ul>
+		{items.map(item => (
+		<li key={item.id}>
+		{item.name}<br/>
+			{item.detail}<br/>
+			<img src={item.coverimage} alt={item.name}/>
+		</li>
+		))}
+	</ul>	
+	</div>
+    )
+}
+
+export default App
